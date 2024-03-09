4f3e9a29f74c3ad822b13b32bfec49798c8ec961
conrad mugabe <conradmugabe@gmail.com>
2024-03-10 02:52:40 +0300

    dev: added more error handlers Added not found error handler Added express-async-errors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
+import 'express-async-errors';
 
 import { currentUserRouter } from './routes/current-user';
 import { signInRouter } from './routes/signin';
 import { signOutRouter } from './routes/signout';
 import { signUpRouter } from './routes/signup';
 import { errorHandler } from './middleware/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const PORT = process.env.PORT || 8000;
 const app = express();
@@ -15,6 +17,10 @@ app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
