c10f93fa49250d1d0564f356b172b4a6ef985043
conrad mugabe <conradmugabe@gmail.com>
2024-03-10 01:32:45 +0300

    feat: added validation to signup route

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,26 @@
 import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/v1/users/signup', async (req: Request, res: Response) => {
-  return res.send('Hi there!');
-});
+router.post(
+  '/api/v1/users/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters'),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    return res.send('Hi there!');
+  }
+);
 
 export { router as signUpRouter };
