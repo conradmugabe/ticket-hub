7d7d083d6c2c2cb4ae7b220b771713c37c329086
conrad mugabe <conradmugabe@gmail.com>
2024-03-12 03:55:34 +0300

    fix: added current user middleware to current user route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,12 +1,15 @@
 import express, { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
 
 import { currentUser } from '../middleware/current-user';
 
 const router = express.Router();
 
-router.get('/api/v1/users/currentuser', async (req: Request, res: Response) => {
-  return res.send({ currentUser: req.currentUser || null });
-});
+router.get(
+  '/api/v1/users/currentuser',
+  currentUser,
+  async (req: Request, res: Response) => {
+    return res.send({ currentUser: req.currentUser || null });
+  }
+);
 
 export { router as currentUserRouter };
