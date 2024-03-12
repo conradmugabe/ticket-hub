164dda4d4496a616ac94a26443fdeb063f6358e1
conrad mugabe <conradmugabe@gmail.com>
2024-03-12 03:41:17 +0300

    feat: added route logic to signout user

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,9 @@ import express, { Request, Response } from 'express';
 const router = express.Router();
 
 router.post('/api/v1/users/signout', async (req: Request, res: Response) => {
-  return res.send('Hi there!');
+  req.session = null;
+
+  return res.send({});
 });
 
 export { router as signOutRouter };
