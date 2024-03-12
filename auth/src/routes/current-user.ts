85aec4844181c7c8ee1e2f4e5a83b7b9b9557076
conrad mugabe <conradmugabe@gmail.com>
2024-03-12 03:50:41 +0300

    dev: added middleware to get user data from session cookie

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,20 +1,12 @@
 import express, { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 
+import { currentUser } from '../middleware/current-user';
+
 const router = express.Router();
 
 router.get('/api/v1/users/currentuser', async (req: Request, res: Response) => {
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
-  }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-
-    return res.send({ currentUser: payload });
-  } catch (error) {
-    return res.send({ currentUser: null });
-  }
+  return res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
