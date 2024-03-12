56a1586525c424c2dd074ea6519972f2e2d81454
conrad mugabe <conradmugabe@gmail.com>
2024-03-12 04:17:52 +0300

    refactor: instantiate express app outside the entry level file

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,33 +1,9 @@
-import 'dotenv/config';
-import express from 'express';
-import 'express-async-errors';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
 
+import { app } from './app';
 import { checkEnvironmentVariables } from './utils/env';
-import { currentUserRouter } from './routes/current-user';
-import { signInRouter } from './routes/signin';
-import { signOutRouter } from './routes/signout';
-import { signUpRouter } from './routes/signup';
-import { errorHandler } from './middleware/error-handler';
-import { NotFoundError } from './errors/not-found-error';
 
 const PORT = process.env.PORT || 8000;
-const app = express();
-app.set('trust proxy', true);
-app.use(express.json());
-app.use(cookieSession({ signed: false, secure: true }));
-
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-app.use(signUpRouter);
-
-app.all('*', async () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
 
 const start = async () => {
   checkEnvironmentVariables();
