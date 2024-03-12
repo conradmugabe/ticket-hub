44b279e259778f6e1ee762ac174fb99f8b4bf650
conrad mugabe <conradmugabe@gmail.com>
2024-03-12 03:40:06 +0300

    feat: added route logic for get current user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,20 @@
 import express, { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/v1/users/currentuser', async (req: Request, res: Response) => {
-  return res.send('Hi there!');
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+
+    return res.send({ currentUser: payload });
+  } catch (error) {
+    return res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
