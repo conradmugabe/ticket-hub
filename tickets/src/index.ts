27934039747dc682293020859bcb7036a7f7944c
conrad mugabe <conradmugabe@gmail.com>
2024-03-15 16:07:59 +0300

    feat: added tickets service

---
+++ tickets/src/index.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import { checkEnvironmentVariables } from '@mors_remorse/ticket-hub-common';
+
+import { app } from './app';
+
+const PORT = process.env.PORT || 8000;
+
+const ENV_VARIABLES = ['TICKETS_MONGO_URI', 'JWT_KEY'];
+
+const start = async () => {
+  checkEnvironmentVariables(ENV_VARIABLES);
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI!);
+    console.log('Connected to mongo database');
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
+};
+
+start();
