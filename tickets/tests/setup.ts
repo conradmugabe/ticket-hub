7009849ae4f09ba5086c3cf490fa7056e51e083a
conrad mugabe <conradmugabe@gmail.com>
2024-03-15 16:48:40 +0300

    test: added test setup file

---
+++ tickets/tests/setup.ts
@@ -0,0 +1,33 @@
+import { afterAll, beforeAll, beforeEach } from 'vitest';
+import {
+  MongoDBContainer,
+  StartedMongoDBContainer,
+} from '@testcontainers/mongodb';
+import mongoose from 'mongoose';
+
+const sixtySeconds = 60 * 1000;
+
+const container = new MongoDBContainer();
+let startedContainer: StartedMongoDBContainer;
+
+beforeAll(async () => {
+  process.env.JWT_KEY = 'TEST KEY';
+
+  startedContainer = await container.start();
+
+  const connectionString = `${startedContainer.getConnectionString()}/test`;
+  await mongoose.connect(connectionString, { directConnection: true });
+}, sixtySeconds);
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+
+  await startedContainer.stop();
+}, sixtySeconds);
