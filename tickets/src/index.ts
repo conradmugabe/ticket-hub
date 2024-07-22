3773d99ac49e7cef0a24da6f99a61963888f3373
conrad mugabe <conradmugabe@gmail.com>
2024-07-22 21:28:01 +0300

    feat: added nats connection logic to nats streaming server

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,16 +1,31 @@
+import { randomBytes } from 'crypto';
+
 import mongoose from 'mongoose';
 import { checkEnvironmentVariables } from '@mors_remorse/ticket-hub-common';
 
 import { app } from './app';
+import { natsClient } from './nats-wrapper';
 
 const PORT = process.env.PORT || 8000;
 
-const ENV_VARIABLES = ['MONGO_URI', 'JWT_KEY'];
+const ENV_VARIABLES = ['MONGO_URI', 'JWT_KEY', 'CLUSTER_ID', 'NATS_URL'];
 
 const start = async () => {
   checkEnvironmentVariables(ENV_VARIABLES);
 
   try {
+    await natsClient.connect(
+      process.env.CLUSTER_ID!,
+      randomBytes(4).toString('hex'),
+      process.env.NATS_URL!
+    );
+    natsClient.client.on('close', () => {
+      console.log('NATS connection closed');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsClient.client.close());
+    process.on('SIGTERM', () => natsClient.client.close());
+
     await mongoose.connect(process.env.MONGO_URI!);
     console.log('Connected to mongo database');
   } catch (error) {
