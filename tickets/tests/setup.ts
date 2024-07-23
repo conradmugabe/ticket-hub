d90f0d5c61d51bac6b118741a65b15853880e200
conrad mugabe <conradmugabe@gmail.com>
2024-07-23 03:40:24 +0300

    feat: added integration tests for nats streaming server publisher events

--- tickets/tests/setup.ts
+++ tickets/tests/setup.ts
@@ -3,19 +3,59 @@ import {
   MongoDBContainer,
   StartedMongoDBContainer,
 } from '@testcontainers/mongodb';
+import {
+  GenericContainer,
+  StartedTestContainer,
+  TestContainer,
+  Wait,
+} from 'testcontainers';
 import mongoose from 'mongoose';
+
+import { natsClient } from '../src/nats-wrapper';
 
 const sixtySeconds = 60 * 1000;
 
-const container = new MongoDBContainer();
-let startedContainer: StartedMongoDBContainer;
+const natsContainer: TestContainer = new GenericContainer(
+  'nats-streaming:0.17.0'
+);
+let startedNatsContainer: StartedTestContainer;
+const nats_client_port = 4223;
+const nats_monitoring_port = 8223;
+const cluster_id = 'TestClusterId';
+
+const mongoDBContainer = new MongoDBContainer();
+let startedMongoDBContainer: StartedMongoDBContainer;
 
 beforeAll(async () => {
   process.env.JWT_KEY = 'TEST KEY';
 
-  startedContainer = await container.start();
+  startedNatsContainer = await natsContainer
+    .withExposedPorts(nats_client_port, nats_monitoring_port)
+    .withCommand([
+      '-p',
+      `${nats_client_port}`,
+      '-m',
+      `${nats_monitoring_port}`,
+      '-hbi',
+      '5s',
+      '-hbt',
+      '5s',
+      '-hbf',
+      '2',
+      '-SD',
+      '-cid',
+      cluster_id,
+    ])
+    .withWaitStrategy(Wait.forLogMessage(/.*Streaming Server is ready*/))
+    .start();
+  startedMongoDBContainer = await mongoDBContainer.start();
 
-  const connectionString = `${startedContainer.getConnectionString()}/test`;
+  const nats_host = startedNatsContainer.getHost();
+  const nats_port = startedNatsContainer.getMappedPort(nats_client_port);
+  const nats_url = `http://${nats_host}:${nats_port}`;
+  await natsClient.connect(cluster_id, 'test', nats_url);
+
+  const connectionString = `${startedMongoDBContainer.getConnectionString()}/test`;
   await mongoose.connect(connectionString, { directConnection: true });
 }, sixtySeconds);
 
@@ -29,5 +69,6 @@ beforeEach(async () => {
 afterAll(async () => {
   await mongoose.disconnect();
 
-  await startedContainer.stop();
+  await startedNatsContainer.stop();
+  await startedMongoDBContainer.stop();
 }, sixtySeconds);
